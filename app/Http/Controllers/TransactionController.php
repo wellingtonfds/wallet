1100a6cb5d28be4f2d89592a92a26c72de41148f
wellington fernandes <wellingtonfds@gmail.com>
2021-05-28 17:21:40 -0300

    update documentation

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -4,17 +4,49 @@ namespace App\Http\Controllers;
 
 use App\Services\CreateTransactionServices;
 use App\Http\Requests\TransactionCreateRequest;
+use Illuminate\Http\JsonResponse;
 
 
 class TransactionController extends Controller
 {
+    /** @OA\Post(
+     *     path="/api/transaction",
+     *       @OA\RequestBody(
+     *         required=true,
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     property="payer",
+     *                     type="int"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="payee",
+     *                     type="int"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="value",
+     *                     type="numeric"
+     *                 ),
+     *
+     *                  example={"payer": 12, "payee": 15, "value":25.12}
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(response="201", description="The user resource",
+     *          @OA\JsonContent(type="object",ref="#/components/schemas/transaction")
+     *     ),
+     *     @OA\Response(response="422", description="Validation error")
+     * )
+     */
     /**
      * Store a newly created resource in storage.
      *
-     * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\JsonResponse
+     * @param TransactionCreateRequest $request
+     * @param CreateTransactionServices $createTransactionServices
+     * @return JsonResponse
      */
-    public function store(TransactionCreateRequest $request, CreateTransactionServices $createTransactionServices)
+    public function store(TransactionCreateRequest $request, CreateTransactionServices $createTransactionServices): JsonResponse
     {
         return $createTransactionServices->create(
             $request->get('payer'),
--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,20 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Transaction extends Model
 {
+    /**
+     * @OA\Schema(
+     *     schema="transaction",
+     *     description="The default resource for an Transaction",
+     *     type="object",
+     *     title="Transaction",
+     *     @OA\Property(property="id", type="int64", description="ID", example="1"),
+     *     @OA\Property(property="payer", type="object",ref="#/components/schemas/user_resource"),
+     *     @OA\Property(property="payee", type="object",ref="#/components/schemas/user_resource"),
+     *     @OA\Property(property="value", type="numeric"),
+     * )
+     */
     use HasFactory;
 
     protected $fillable = ['payer', 'payee', 'value'];
 
 
-    public function userPayer()
+    public function userPayer(): BelongsTo
     {
         return $this->belongsTo(User::class, 'payer');
     }
 
-    public function userPayee()
+    public function userPayee(): BelongsTo
     {
         return $this->belongsTo(User::class, 'payee');
     }
